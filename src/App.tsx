3dae46e9821f61b997d04b315b62f891c1481c5e
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-06-06 16:38:17 +0300

    Create Navbar cmp

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
+import { Navbar } from './cmps/Navar'
 
 function App() {
 
   return (
     <>
-      <h1 className="text-1xl font-bold underline">
-        Hello world!
-      </h1>
-      <button
-        className="btn btn-outline">
-        Button
-      </button>
+      <Navbar />
     </>
   )
 }
