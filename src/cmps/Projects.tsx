543d686b0d0162c9dae2b6bdcd2a1b79ec6671ab
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-06-11 14:24:30 +0300

    Add 1 more project to the projects section

--- src/cmps/Projects.tsx
+++ src/cmps/Projects.tsx
@@ -27,17 +27,17 @@ export function Projects() {
     >
       <h1 className='gradient-text text-center capitalize text-3xl tracking-wider font-semibold my-4'>projects</h1>
       <div className='flex flex-col sm:flex-row gap-5 justify-between mt-8'>
-        <a href='https://jobtracker-x1u9.onrender.com' target="_blank" className="h-64 sm:w-96 rounded-md bg-gradient-to-r from-gradient-purple via-gradient-red to-gradient-orange1 p-1 hover:scale-105 duration-300">
+        <a href='https://jobtracker-x1u9.onrender.com' target="_blank" className="sm:h-72 sm:w-96 rounded-md bg-gradient-to-r from-gradient-purple via-gradient-red to-gradient-orange1 p-1 hover:scale-105 duration-300">
           <div className="flex flex-col h-full w-full  bg-bg-dark p-2">
             <div className='bg-white/5 size-14 flex items-center justify-center rounded-md '>
-              <img src="src/assets/jobtracker.png" alt="" />
+              <img src="src/assets/jobtracker.png" alt="app logo" />
             </div>
             <div className='flex items-center mt-2 text-white gap-2'>
               <h1 className=" capitalize ">jobTracker</h1>
               <FaLink className='text-lg' />
             </div>
-            <p className='text-gray text-sm mt-2'>📰 Job Tracker streamlines your job search by efficiently organizing a watch list. Easily manage the jobs you've applied for.</p>
-            <div className='flex flex-wrap text-white text-sm gap-3 mt-2 capitalize items-center'>
+            <p className='text-gray text-sm mt-4'>📰 Job Tracker streamlines your job search by efficiently organizing a watch list. Easily manage the jobs you've applied for.</p>
+            <div className='flex flex-wrap text-white text-sm gap-3 mt-auto capitalize items-center'>
               <div className='bg-white/5 p-1 rounded-md'>
                 <span>react</span>
               </div>
@@ -59,17 +59,17 @@ export function Projects() {
             </div>
           </div>
         </a>
-        <a href='https://comfy-store-xaft.onrender.com' target="_blank" className="h-64 sm:w-96 rounded-md bg-gradient-to-r from-gradient-purple via-gradient-red to-gradient-orange1 p-1 hover:scale-105 duration-300">
+        <a href='https://comfy-store-xaft.onrender.com' target="_blank" className="sm:h-72 sm:w-96 rounded-md bg-gradient-to-r from-gradient-purple via-gradient-red to-gradient-orange1 p-1 hover:scale-105 duration-300">
           <div className="flex flex-col h-full w-full  bg-bg-dark p-2">
             <div className='bg-white/5 size-14 flex items-center justify-center p-2  rounded-md '>
-              <img src="src/assets/comfystore.png" alt="" />
+              <img src="src/assets/comfystore.png" alt="app logo" />
             </div>
             <div className='flex items-center mt-2 text-white gap-2'>
               <h1 className=" capitalize ">comfyStore</h1>
               <FaLink className='text-lg' />
             </div>
-            <p className='text-gray text-sm mt-2'>🪑 ComfyStore app offers stylish furniture for every room. Shop and customize your perfect pieces with ease.</p>
-            <div className='flex flex-wrap text-white text-sm gap-3 mt-2 capitalize items-center'>
+            <p className='text-gray text-sm mt-4'>🪑 ComfyStore  offers stylish furniture for every room. Shop and customize your perfect pieces with ease.</p>
+            <div className='flex flex-wrap text-white text-sm gap-3 mt-auto  capitalize items-center'>
               <div className='bg-white/5 p-1 rounded-md'>
                 <span>react</span>
               </div>
@@ -84,6 +84,26 @@ export function Projects() {
               </div>
               <div className='bg-white/5 p-1 rounded-md'>
                 <span>tailwind</span>
+              </div>
+            </div>
+          </div>
+        </a>
+        <a href='https://globetrek.netlify.app/' target="_blank" className="sm:h-72 sm:w-96 rounded-md bg-gradient-to-r from-gradient-purple via-gradient-red to-gradient-orange1 p-1 hover:scale-105 duration-300">
+          <div className="flex flex-col h-full w-full  bg-bg-dark p-2">
+            <div className='bg-white/5 size-14 flex items-center justify-center p-2  rounded-md '>
+              <img src="src/assets/globetrek.png" alt="app logo" />
+            </div>
+            <div className='flex items-center mt-2 text-white gap-2'>
+              <h1 className=" capitalize ">globeTrek</h1>
+              <FaLink className='text-lg' />
+            </div>
+            <p className='text-gray text-sm mt-4'>🌎 GlobeTrek app designed to help users plan and discover their next adventure trek. Users can explore various destinations, view details about each trek, and access helpful resources for planning their journey.</p>
+            <div className='flex flex-wrap text-white text-sm gap-3 mt-auto capitalize items-center'>
+              <div className='bg-white/5 p-1 rounded-md'>
+                <span>react</span>
+              </div>
+              <div className='bg-white/5 p-1 rounded-md'>
+                <span>scss</span>
               </div>
             </div>
           </div>
