5a9d2b0f7dd032e01b6bd5aa81334f63eab2b855
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-06-18 11:28:12 +0300

    Add linkedin and github icons to contact section

--- src/cmps/Contact.tsx
+++ src/cmps/Contact.tsx
@@ -1,17 +1,8 @@
-import React, { useRef, useState } from 'react';
-import emailjs from '@emailjs/browser';
+import React, { useRef, useState } from 'react'
+import emailjs from '@emailjs/browser'
 import { toast } from 'react-toastify'
-// type Email = {
-//   name: string,
-//   mail: string,
-//   txt: string
-// }
-
-// let emailInputs: Email = {
-//   name: '',
-//   mail: '',
-//   txt: ''
-// }
+import { FaLinkedin } from "react-icons/fa"
+import { FaGithub } from "react-icons/fa"
 
 export function Contact() {
   const form = useRef<HTMLFormElement | null>(null)
@@ -43,6 +34,10 @@ export function Contact() {
   return (
     <section className='mt-12 border border-t-gray' id='contact'>
       <h1 className='gradient-text text-center capitalize text-3xl tracking-wider font-semibold my-4 flex justify-center'>contact</h1>
+      <div className='flex justify-center items-center text-white gap-4'>
+        <a href="https://www.linkedin.com/in/amir-yankolovich/" target="_blank"><FaLinkedin className='text-2xl hover:text-linkedin duration-100' /></a>
+        <a href="https://github.com/Amir2210" target="_blank"><FaGithub className='text-2xl hover:text-github duration-100' /></a>
+      </div>
       <form className='flex flex-col items-center' ref={form} onSubmit={sendEmail}>
         <input onChange={(ev) => setName(ev.target.value)} value={name} placeholder='Full Name' className='outline outline-offset-1 outline-2 outline-gradient-purple p-2 rounded-sm border-none w-full max-w-xs my-4 bg-bg-dark text-white' type="text" name="from_name" required />
         <input onChange={(ev) => setEmail(ev.target.value)} value={email} placeholder='Email Address' className='outline outline-offset-1 outline-2 outline-gradient-purple p-2 rounded-sm border-none w-full max-w-xs my-4 bg-bg-dark text-white' type="email" name="from_email" required />
