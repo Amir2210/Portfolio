f0c5905e281554ca6e6e08abf48804a113936f22
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-07-08 12:24:49 +0300

    Add resume to the about section

--- src/cmps/About.tsx
+++ src/cmps/About.tsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion"
 import avatar from '../assets/imgs/avatar.png'
+import resume from '../assets/certifications/resume.pdf'
 export function About() {
 
   const fadeInAnimations = {
@@ -31,14 +32,14 @@ export function About() {
           <h2 className='capitalize mt-4 text-xl sm:text-3xl justify-center flex sm:justify-start'>full stack / frontend developer.</h2>
           <p className='mt-4 tracking-wider text-gray text-center sm:text-left'>Full Stack programmer specializing in Frontend development. I focus on creating engaging user experiences and solving complex problems. I really like reading books and sports</p>
           <div className='hidden sm:flex sm:flex-wrap gap-5 my-4 items-center'>
-            <div className='capitalize cursor-pointer purple-outline'>download CV</div>
+            <div className='capitalize cursor-pointer purple-outline'><a href={resume} target='_blank'>download CV</a></div>
             <div className='capitalize cursor-pointer purple-outline'><a href="#certifications">licenses & certifications</a></div>
           </div>
         </div>
         <div>
           <img src={avatar} alt="avatar" />
           <div className='flex flex-wrap justify-center sm:hidden gap-5 my-4 items-center'>
-            <div className='capitalize cursor-pointer purple-outline'>download CV</div>
+            <div className='capitalize cursor-pointer purple-outline'><a href={resume} target='_blank'>download CV</a></div>
             <div className='capitalize cursor-pointer purple-outline'><a href="#certifications">licenses & certifications</a></div>
           </div>
         </div>
