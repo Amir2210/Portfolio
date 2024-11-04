d18876ffb61fc97a77e01d6be0508d629c93326f
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-11-04 12:39:14 +0200

    Add new Certification

--- src/cmps/Certifications.tsx
+++ src/cmps/Certifications.tsx
@@ -4,6 +4,7 @@ import bootcamp from '../assets/certifications/bootcamp.pdf'
 import electronic from '../assets/certifications/electronic.pdf'
 import web from '../assets/certifications/web.pdf'
 import udemy from '../assets/certifications/udemy.pdf'
+import udemy2 from '../assets/certifications/udemy2.pdf'
 import { FaFilePdf } from "react-icons/fa6"
 import { motion } from "framer-motion"
 
@@ -35,12 +36,12 @@ export function Certifications() {
     >
       <h1 className='gradient-text text-center capitalize text-3xl tracking-wider font-semibold my-4'>licenses & certifications</h1>
       <div className='flex flex-wrap justify-center text-white mt-8 sm:gap-4 gap-6'>
-        <a href={colonel} target='_blank' className='capitalize cursor-pointer purple-outline flex items-center justify-center gap-2'>
-          <h3>base military honor from colonel</h3>
+        <a href={udemy} target='_blank' className='capitalize cursor-pointer purple-outline flex items-center justify-center gap-2'>
+          <h3>udemy - HTML & CSS</h3>
           <FaFilePdf className='text-xl' />
         </a>
-        <a href={major} target='_blank' className='capitalize cursor-pointer purple-outline flex items-center justify-center gap-2'>
-          <h3>military honor from Major</h3>
+        <a href={udemy2} target='_blank' className='capitalize cursor-pointer purple-outline flex items-center justify-center gap-2'>
+          <h3>udemy - React, next.js & TypeScript</h3>
           <FaFilePdf className='text-xl' />
         </a>
         <a href={bootcamp} target='_blank' className='capitalize cursor-pointer purple-outline flex items-center justify-center gap-2'>
@@ -55,10 +56,15 @@ export function Certifications() {
           <h3>web development Military course</h3>
           <FaFilePdf className='text-xl' />
         </a>
-        <a href={udemy} target='_blank' className='capitalize cursor-pointer purple-outline flex items-center justify-center gap-2'>
-          <h3>udemy HTML & CSS course</h3>
+        <a href={colonel} target='_blank' className='capitalize cursor-pointer purple-outline flex items-center justify-center gap-2'>
+          <h3>base military honor from colonel</h3>
           <FaFilePdf className='text-xl' />
         </a>
+        <a href={major} target='_blank' className='capitalize cursor-pointer purple-outline flex items-center justify-center gap-2'>
+          <h3>military honor from Major</h3>
+          <FaFilePdf className='text-xl' />
+        </a>
+
       </div>
     </motion.section>
   )
